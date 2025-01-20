944a925ce777b7cad2dfd7f0e54c8816eaba6dce
amirisback <faisalamircs@gmail.com>
2025-01-20 21:51:50 +0700

    Update: trial

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,9 +2,11 @@ import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
@@ -20,17 +22,19 @@ fun App() {
         Row(
             modifier = Modifier.padding(dimen_16dp)
         ) {
-            Column {
+            Column(
+                modifier = Modifier.align(Alignment.CenterVertically)
+            ) {
 
-//            Button(onClick = {
-//                text = "Hello, Desktop!"
-//            }) {
-//                Text(text)
-//            }
-//
-//            Text(
-//                text = "Oke"
-//            )
+                Button(onClick = {
+                    text = "Hello, Desktop!"
+                }) {
+                    Text(text)
+                }
+
+                Text(
+                    text = "Oke"
+                )
 
                 Greeting(name = "Desktop")
 
