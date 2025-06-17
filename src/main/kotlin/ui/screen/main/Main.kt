14514964ed6903f70d5167ac8e37d86a2e9346f7
amirisback <faisalamircs@gmail.com>
2025-06-17 16:07:32 +0700

    update: add layout input

--- src/main/kotlin/ui/screen/main/Main.kt
+++ src/main/kotlin/ui/screen/main/Main.kt
@@ -1,41 +1,74 @@
 package ui.screen.main
 
 import androidx.compose.desktop.ui.tooling.preview.Preview
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import ui.theme.dimen_16dp
+import util.PROMPT_SHUTDOWN_CANCEL
+import util.runPrompt
+import util.shutdownPrompt
 
 @Composable
 @Preview
 fun App() {
-    var text by remember { mutableStateOf("Hello, World!") }
 
     MaterialTheme {
 
         Row(
-            modifier = Modifier.padding(dimen_16dp)
+            modifier = Modifier.padding(dimen_16dp).fillMaxSize()
         ) {
             Column(
-                modifier = Modifier.align(Alignment.CenterVertically)
+                modifier = Modifier.align(Alignment.CenterVertically).fillMaxSize()
             ) {
 
-                Button(onClick = {
-                    text = "Hello, Desktop!"
-                }) {
-                    Text(text)
-                }
+                Text(
+                    text = "Input your shutdown timer in seconds",
+                    modifier = Modifier.fillMaxWidth()
+                )
 
-                Text(text = "Oke")
-                Greeting(name = "Desktop")
+                var textField by rememberSaveable { mutableStateOf("") }
+
+                OutlinedTextField(
+                    modifier = Modifier.fillMaxWidth().padding(top = 16.dp, bottom = 16.dp),
+                    value = textField,
+                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+                    onValueChange = {
+                        textField = it
+                    }
+                )
+                Spacer(Modifier.weight(1f))
+                Row() {
+                    Button(
+                        modifier = Modifier.weight(1f).padding(end = 8.dp),
+                        onClick = {
+                            runPrompt(shutdownPrompt(textField.toInt()))
+                        }) {
+                        Text("Set Shutdown Timer")
+                    }
+
+                    Button(
+                        modifier = Modifier.weight(1f).padding(start = 8.dp),
+                        onClick = {
+                            runPrompt(PROMPT_SHUTDOWN_CANCEL)
+                        }) {
+                        Text("Reset")
+                    }
+                }
 
             }
         }
@@ -43,13 +76,6 @@ fun App() {
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
 fun main() = application {
     Window(
--- src/main/kotlin/util/WindowsPrompt.kt
+++ src/main/kotlin/util/WindowsPrompt.kt
@@ -4,5 +4,9 @@ fun runPrompt(rule: String) {
     Runtime.getRuntime().exec(rule)
 }
 
+fun shutdownPrompt(second: Int): String {
+    return "shutdown -s -t $second"
+}
+
 const val PROMPT_SHUTDOWN_SET = "shutdown -s -t 3600"
 const val PROMPT_SHUTDOWN_CANCEL = "shutdown -a"
